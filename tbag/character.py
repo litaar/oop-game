7ccd9112c63f74a0c00b2f519c3a0183678582c9
litaar <lalitahill77@gmail.com>
2024-08-22 14:34:55 +1000

    # first commit # began figuring out my main.py and the appropriate loops and if statements # Started creating the map of the game # Started the Enemy class and figuring out an unresolved error # Working out a way to add the levelling system using if statements and a boolean to check if player is alive

--- tbag/character.py
+++ tbag/character.py
@@ -2,13 +2,24 @@ import random
 
 # Add a boolean to represent while Enemy == True
 
+food_list = ["Burger", "Chips", "Soda"]
 
-food_list = str["Burger", "Chips", "Soda"]
+# Add character attribute here
+#     self.character = None
 
 
 class Enemy:
+    a = 0
     print("{Customer}: Where is my food!")
-    print("{Customer}: I want my"(print(random.choice(food_list))))
+    print("{Customer}: I want my")
+    print(random.choice(food_list))
+# Add an if statement to check if player has passed the level
+   if dead == False:
+    print("{Customer}: ☺")
+    else:
+    print("(╬≖_≖)")
+
+
 
 
 
--- tbag/location.py
+++ tbag/location.py
@@ -1,12 +1,35 @@
-#This will represent the maps the player will play with
+# This will represent the maps the player will play with
 class Map:
-# Add constructor to class
+    # Add constructor to class
     def __init__(self, map_name, map_description):
         # Add link_cave method here
         self.map_name = map_name
         self.description = map_description
-        # Add character attribute here
-        self.character = None
 
+         # Here is a method to get the description of the cave:
+        def get_description(self):
+            return self.description
+
+        # Here is a method to set the description of the cave:
+        def set_description(self, map_description):
+            self.description = map_description
+
+        def get_name(self):
+            return self.map_name
+
+        def set_name(self, map_name):
+            self.cave_name = name
+
+        # Give map a description
+        def describe(self):
+            print(self.get_description())
+
+        # get and print details of cave (name, description, directions)
+        def get_details(self):
+            print(self.cave_name)
+            print(self.description)
+            for direction in self.linked_caves:
+                cave = self.linked_caves[direction]
+                print("The " + cave.get_name() + " is " + direction)
 
     attribute: health
